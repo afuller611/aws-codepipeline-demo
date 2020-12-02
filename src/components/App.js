dac78a4b91679c1be1da5434fbdec5fbc7316eea
Andre Fuller <afuller611@gmail.com>
2020-12-02 16:11:15 -0700

    make it donuty

---
+++ src/components/App.js
@@ -0,0 +1,40 @@
+import React, { useState } from 'react';
+import { Grid, Typography, Tabs, Tab, AppBar } from '@material-ui/core';
+import { doughnutTypes, donuts } from './data'; // TODO -- get from API
+import donutImage from './donut-sample.png'
+
+const App = (props) => {
+  const [value, setValue] = useState("All")
+  console.log(value)
+  return (
+    <div style={{ padding: 40 }}>
+      <Grid container justify="center">
+        <Typography variant="h1" style={{ color: "#abccd4" }}>{"Get Yo' Donuts Here"}</Typography>
+        <AppBar style={{ boxShadow: "none" }} position="static">
+          <Tabs style={{ color: "black", backgroundColor: "white", padding: 0 }} centered value={value} onChange={(e, newValue) => setValue(newValue)} aria-label="simple tabs example">
+            <Tab value={"All"} label="All" />
+            {doughnutTypes.map((type, index) => (
+              <Tab value={type} label={type} key={index} />
+            ))}
+          </Tabs>
+        </AppBar>
+        <Grid justify="center" container spacing={2} style={{ padding: 20 }}>
+          {donuts.filter(donut => {
+            if (value === "All") return true
+            else return donut.Type === value
+          }).map((donut, index) =>
+            (
+              // TODO -- get from API and figure out images
+              // Probs make a common component for a donut
+              <Grid key={index} container justify="center" item xs={3}>
+                <img src={donutImage} alt={donut.Name} />
+                {donut.Name}
+              </Grid>
+            ))}
+        </Grid>
+      </Grid>
+    </div>
+  )
+}
+
+export default App;
---
+++ src/components/data.js
@@ -0,0 +1,56 @@
+export const doughnutTypes = [
+    "Iced", "Glazed", "Filled", "Cake", "Chocolate"
+]
+
+export const donuts = [
+    {
+        Name: "Something",
+        Path: "../donut-sample.png",
+        Type: "Iced"
+    },
+    {
+        Name: "Something",
+        Path: "./donut-sample.png",
+        Type: "Iced"
+    },
+    {
+        Name: "Something",
+        Path: "./donut-sample.png",
+        Type: "Iced"
+    },
+    {
+        Name: "Something",
+        Path: "./donut-sample.png",
+        Type: "Glazed"
+    },
+    {
+        Name: "Something",
+        Path: "./donut-sample.png",
+        Type: "Filled"
+    },
+    {
+        Name: "Something",
+        Path: "./donut-sample.png",
+        Type: "Filled"
+    },
+    {
+        Name: "Something",
+        Path: "./donut-sample.png",
+        Type: "Cake"
+    },
+    {
+        Name: "Something",
+        Path: "./donut-sample.png",
+        Type: "Cake"
+    },
+    {
+        Name: "Something",
+        Path: "./donut-sample.png",
+        Type: "Cake"
+    },
+    {
+        Name: "Something",
+        Path: "./donut-sample.png",
+        Type: "Chocolate"
+    }
+]
